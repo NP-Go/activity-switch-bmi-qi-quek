6d28ab69352fbb22aabdc9dd2f506597a2a92111
narwhal04 <q.quek7@gmail.com>
2022-03-31 21:19:42 +0800

    added comments

--- switchBMI.go
+++ switchBMI.go
@@ -1,5 +1,38 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
+var weight, height float64
+
 func main() {
-	//Insert Code Here
+
+	fmt.Println("This app will determine your BMI level")
+	fmt.Print("Please input your weight:")
+	fmt.Scan(&weight)
+	fmt.Print("Please input your height:")
+	fmt.Scan(&height)
+
+	bmiIndi := weight / (math.Pow((height / 100), 2))
+
+	fmt.Printf("%.02f, %T\n", bmiIndi, bmiIndi)
+
+	switch { // when there is conditional comparison, switch is empty
+	//if it is purely categorical variable, switch is filled with variable
+	case bmiIndi < 18.5: // examples of conditional comparison
+		fmt.Println("Underweight")
+	case bmiIndi >= 18.5 && bmiIndi < 24.9:
+		fmt.Println("Healthy Weight")
+	case bmiIndi >= 24.9 && bmiIndi < 29.9:
+		fmt.Println("OverWeight")
+	case bmiIndi >= 30 && bmiIndi < 34.9:
+		fmt.Println("Obese")
+	case bmiIndi >= 35 && bmiIndi < 39.9:
+		fmt.Println("Severely Obese")
+	default:
+		fmt.Println("Morbidly Obese")
+	}
+
 }
